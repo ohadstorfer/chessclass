c8d724206c521dcf8a54792dd72160a6b6c04388
ohadstorfer <ohad.storfer@gmail.com>
2023-08-27 12:50:19 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+class Pawn:
+    color=""
+    position_col=int(0)
+    position_row=int(0)
+    def kill(self):
+        pass
+    def get_killed(self):
+        pass
+    def crowned(self):
+        pass
+    def movement(self):
+        self.postions_col+=1
+
+p1=Pawn()
+p1.color = "white"
+p1.position_row = 5
+p2=Pawn
+p2.color = "black"
+p2.position_row = 63
